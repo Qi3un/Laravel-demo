88c43fa780b064617b766f4cc64d3ee80c32919b
tr0ubl3sh00t3r <qs5@rice.edu>
2017-12-14 15:13:39 -0600

    config url

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://cryptic-wildwood-37905.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
